b85151c2e51dd0e40d3f5fb364ca7da198413205
Larissa Amancio <larissa.amancio@outlook.com>
2017-03-26 17:08:35 -0300

    conseguindo listar

--- src/components/invites/Okrs.js
+++ src/components/invites/Okrs.js
@@ -5,24 +5,27 @@ import Header from 'components/invites/Header';
 class Okrs extends React.Component {
 
   componentDidMount() {
-    //const { request, params: { uid } } = this.props;
-    //const invite = profile.getIn(['invites', uid]);
-    //if (!invite) {
-    //request({ uid })
-    //}
+    const { profile, params: { uid } } = this.props;
+    const invite = profile.getIn(['invites', uid]);
+    if (!invite) {
+    profile({ uid })
+    }
   }
 
   render() {
     const { profile, params: { uid } } = this.props;
     const invite = profile.getIn(['invites', uid]);
-    debugger;
+    const okrs = profile.getIn(['invites', uid, 'feedback', '1239987666', 'okrs' ]);
+    const descriptions = okrs.map((description, key) => (<div key={key}>{okrs.description}</div>)).toList().toJS()
     return (
       <div>
-        <Header invite={invite} />
-
+        <Header invite={ invite } />
+        <div>{ descriptions }</div>
       </div>
     );
   }
 }
 
 export default connect(({ profile }) => ({ profile }))(Okrs);
+
+// feedbacks.map((feedback, key) => (<div key={key}>{feedback.description}</div>)).toList().toJS()
